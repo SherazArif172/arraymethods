974ef29c0e3b9204b418fa27939ce0c96e9724e2
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-27 15:59:02 +0500

    added to reversed

---
+++ js/array/toreversed27.js
@@ -0,0 +1,7 @@
+const arr = [1, 2, 2, 3, 4, 4, 5, 6, 6];
+
+console.log(arr.toReversed());
+console.log(arr);
+
+//does not modifies the origional aray
+// reverse the prder of values of the an array
