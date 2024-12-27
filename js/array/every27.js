4764826599e6fc2d2327a9ec95b0813a7d61a480
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-27 15:22:37 +0500

    added lastindex and every methods

---
+++ js/array/every27.js
@@ -0,0 +1,8 @@
+const arr = [1, 2, 2, 3, 4, 4, 5, 6, 6];
+
+const newArr = arr.every((x) => x > 6);
+console.log(newArr);
+
+// to check fr all the entries of the array
+// if one is false it will return false
+// to return true all should be true
---
+++ js/array/lastindexof27.js
@@ -0,0 +1,6 @@
+const arr = [1, 2, 2, 3, 4, 4, 5, 6, 6];
+
+console.log(arr.lastIndexOf(6));
+
+// if we have multiple same values in a array , it will return the last index of the matched value
+// does not modifies the origional array
