291aefa4339c902816ed6c0b64b3198652bf3b6f
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-27 16:51:29 +0500

    added with method

--- js/array/valueof27.js
+++ js/array/valueof27.js
@@ -3,6 +3,8 @@ const arr = [1, 2, 2, 3, 4, 4, 5, 6, 6];
 const arr2 = arr.valueOf();
 
 console.log(arr2);
+console.log(arr);
 
 // method returns the array itself
 // does not modifies thee origionl array
+// modifies the origional array
---
+++ js/array/with27.js
@@ -0,0 +1,8 @@
+const arr = [1, 2, 2, 3, 4, 4, 5, 6, 6];
+
+const arr2 = arr.with(3, 88);
+
+console.log(arr2);
+
+// 3 is index to be targeted
+// 88 value will be replaced by the value of index 3
