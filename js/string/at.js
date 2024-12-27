83d740d05d75153787c36fded65b507127ddecbb
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-27 21:48:58 +0500

    added charat

--- js/string/at.js
+++ js/string/at.js
@@ -1,6 +1,7 @@
 const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-console.log(str.at(25));
+console.log(str.at(-5));
 
 // gives the value of passed index
 // does not modifies the origional string
+// it takes nagative values in the reverse order
---
+++ js/string/charAt.js
@@ -0,0 +1,5 @@
+const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+console.log(str.charAt(-1));
+
+// it is same like at() but main difference btween these two is that when a nagative values is passed it it it gives empety string and at() gives value in reverse order
