d97c2cabc5ae60aaf280372a40f8d81225131103
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-27 17:13:39 +0500

    added flat

---
+++ js/array/flat27.js
@@ -0,0 +1,11 @@
+const arr = [
+  [1, 2, 2],
+  [3, 4, 4],
+  [5, 6, 6],
+];
+
+console.log(arr.flat());
+console.log(arr);
+
+// will concat all the sub arrays into a single array / make flate the mountain
+// does not modifies the origional array
