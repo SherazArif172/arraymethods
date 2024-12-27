5cd3f27f56c76077f4d03b1b2d3181e516b86211
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-27 15:55:23 +0500

    added tosorted

--- js/array/sort27.js
+++ js/array/sort27.js
@@ -1,3 +1,8 @@
 const arr = ["sheraz", "asim", "umair"];
 
 console.log(arr.sort());
+
+console.log(arr);
+
+// it modifies the origional array
+// the array values will be sorted by the capital
---
+++ js/array/tosorted27.js
@@ -0,0 +1,7 @@
+const arr = ["sheraz", "asim", "umair"];
+
+console.log(arr.toSorted());
+console.log(arr);
+
+// does not modifies the origional array
+// sort the values alphabeticaly
