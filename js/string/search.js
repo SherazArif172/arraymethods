6964233f6c40aeedb30a668dc00b35d110627ad1
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-31 14:23:18 +0500

    added search

--- js/string/search.js
+++ js/string/search.js
@@ -1,4 +1,5 @@
-const str = "i am sheraz arif from Pakistan";
+const str = "i am sheraz arif from Pakistan am";
 
 console.log(str.search("Pakistann")); // if it is not in string it returns -1
 console.log(str.search("Pakistan")); //it returns the
+console.log(str.search(/am/g)); //it returns the
