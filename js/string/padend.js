6cc5decfccdeab3964f3bdfee2299c17b13f2295
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-31 13:33:51 +0500

    added padend

---
+++ js/string/padend.js
@@ -0,0 +1,7 @@
+const str = "5d";
+
+console.log(str.padEnd(5, "x"));
+
+// 5 = defines the number uptil the padding will be added
+// x = is the fill string will be filled in the padding
+// returns the string
