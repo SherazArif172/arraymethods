2dcd5c9493086fffd42b5362dd50cb1ab4e9446e
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-31 11:20:25 +0500

    added indexof

---
+++ js/string/indexof.js
@@ -0,0 +1,7 @@
+const str = "ABCD EF GHI JK L MNOPQ  RST UVWX Y Z";
+
+console.log(str.indexOf("Z"));
+console.log(str.indexOf("MNOPQ", 17));
+
+// gives the index og a specific character
+// gives the index of a specific string starting from a specific index
