058864a05e47e0fa0257d2ff2d351ffa3cfefe69
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-31 15:20:45 +0500

    added tolowercaselocal

---
+++ js/string/tolocallowercase.js
@@ -0,0 +1,6 @@
+const str = "Hello Pakistan , Where are you";
+
+console.log(str.toLocaleLowerCase());
+
+// it coverts all the string to lower case using the current language of browser
+// same as the toLowerCase() method but it uses the current language of browser to convert the string to lower case
