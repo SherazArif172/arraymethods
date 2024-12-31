0c9fe10200e762270a078bc9b03ec2d0d8265495
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-31 16:14:20 +0500

    added construcor in array

---
+++ js/array/constructor.js
@@ -0,0 +1,7 @@
+const arrr = new Array(3); // will create a array having 3 length but empty
+const arr = new Array("Usman Bhai", "Umair Bhai"); // 1
+const Arr = ["Usman Bhai", "Umair Bhai"]; // 2
+
+console.log(arrr);
+console.log(Arr);
+console.log(arr);
