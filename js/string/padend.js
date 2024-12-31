e8bdc5c397fd4fc95dca4a16c861ad7032a5d78b
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-31 13:37:00 +0500

    added padstart

--- js/string/padend.js
+++ js/string/padend.js
@@ -2,6 +2,6 @@ const str = "5d";
 
 console.log(str.padEnd(5, "x"));
 
-// 5 = defines the number uptil the padding will be added
+// 5 = defines the number uptil the padding will be added in the end
 // x = is the fill string will be filled in the padding
 // returns the string
---
+++ js/string/padstart.js
@@ -0,0 +1,7 @@
+const str = "ABCD EF GHI JK L MNOPQ  RST UVWX Y Z";
+
+console.log(str.padStart(40, "b"));
+
+// 5 = defines the number uptil the padding will be added in the start
+// x = is the fill string will be filled in the padding
+// returns the string
