11d9f8bb513e6e68bc7e2853dc3762be8afff432
SherazArif172 <sherazarifofficial@gmail.com>
2024-12-31 11:15:24 +0500

    added includes in string

---
+++ js/string/includes.js
@@ -0,0 +1,7 @@
+const str = "ABCD EF GHI JK L MNOPQ  RST UVWX Y Z";
+
+console.log(str.includes("Z", 35));
+console.log(str.includes("UVWX"));
+
+// checks if the character is in the string it i'll give true else false
+// 35 = index of the given character
