4667b85e32c565f9e59b31f4d17ef90895c272e1
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-01 17:57:15 +0500

    added constructor of object

---
+++ js/object/methods/costructor.js
@@ -0,0 +1,12 @@
+const obj = new Object();
+const simpleObj = {
+  name: "sheraz",
+  email: "sherazarifofficial@gmail.com",
+  age: 25,
+};
+
+obj.name = "sheraz";
+
+console.log(obj);
+console.log(simpleObj.name);
+console.log(simpleObj["name"]);
