9e5731299d533079d8412fe447ff1009e65b211c
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-01 12:07:39 +0500

    added blink

--- js/string/big.js
+++ js/string/big.js
@@ -4,3 +4,4 @@ console.log(str.big());
 
 // creates the big tag accound given string
 // big tag is for bigger text but not supportes now in html
+// depricated
---
+++ js/string/blink.js
@@ -0,0 +1,7 @@
+const str = "happy new year ";
+
+console.log(str.blink());
+
+// creates the blink tag accound given string
+// depricated
+// not supported in html now
