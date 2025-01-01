a61d52b5a31a1f0321eb3a4bc2c41480ec57e21d
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-01 14:07:42 +0500

    checked by umair bhai

--- js/array/foreach27.js
+++ js/array/foreach27.js
@@ -1,11 +1,26 @@
 const arr = [1, 2, 2, 3, 4, 4, 5, 6, 6];
 
-const arr2 = arr.forEach((x, y) => {
-  console.log(x, y);
-});
-console.log(arr2);
+// const arr2 = arr.forEach((x, y) => {
+//   console.log(x, y);
+// });
+// console.log(arr2);
 
-console.log(arr);
+// console.log(arr);
 
 // will run once for each element till end
 // will noe modify the origional array
+
+Array.prototype.myForEach = function (callbackfn) {
+  for (let i = 0; i < this.length; i++) {
+    const element = this[i];
+
+    callbackfn(element, i, this);
+  }
+};
+
+// arr.forEach((value, index, arr) => {
+//   console.log({ value, index, arr });
+// });
+arr.myForEach((value, index, arr) => {
+  console.log({ value, index, arr });
+});
