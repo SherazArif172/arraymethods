e236da2f8319bfa01b64259af76a9e6023cc4c35
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-01 12:50:46 +0500

    added constructor

---
+++ js/string/constructor.js
@@ -0,0 +1,7 @@
+const newStrObj = new String("Happy new year");
+const newStr = String("Happy new year");
+
+console.log(newStrObj);
+console.log(typeof newStrObj);
+console.log(newStr);
+console.log(typeof newStr);
