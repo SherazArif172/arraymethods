ce084a9a1ea9fd501935ba756e0a100d3208aa23
SherazArif172 <sherazarifofficial@gmail.com>
2025-01-03 11:20:01 +0500

    added at logic

--- js/array/methods/at.js
+++ js/array/methods/at.js
@@ -1,22 +1,13 @@
 const arr = [1, 2, 2, 3, 4, 4, 5, 6, 6];
 
-// const arrAt = arr.at(4);
+// const arrAt = arr.at(45);
 // console.log(arrAt);
 
 // console.log(arr);
 
-// does not modifies the origiona array
-// any value can be extracted from array using this
-
-Array.prototype.myAt = function () {
-  for (let i = 0; i < this.length; i++) {
-    const element = this[i];
-    if (this[i] == element) {
-    }
-    // callbackfn(element);
-  }
+Array.prototype.myAt = function (index) {
+  const idx = index < 0 ? this.index + index : index;
+  return this[idx];
 };
 
-arr.myAt((i) => {
-  console.log(i);
-});
+console.log(arr.myAt(44));
